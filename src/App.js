9279216da3dc4d752e74281010cd7d63118e2fc9
overaction <kmc04@naver.com>
2020-01-20 22:14:17 +0900

    Food Component with props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import Potato from "./Potato"; 
+
+function Food({fav}) {
+  return <h1>I like {fav}</h1>
+}
+/*
+function Food(props) {
+  return <h1>I like {props.fav}</h1>
+}
+*/
+
 function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <Potato />
+      <Food fav="kimchi" />
+      <Food fav="potato"></Food>
     </div>
   );
 }
