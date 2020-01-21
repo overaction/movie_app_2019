86b33afb5dcbb55da8cca8a2e9a8dee49833f1f0
overaction <kmc04@naver.com>
2020-01-21 20:04:18 +0900

    settimeout

--- src/App.js
+++ src/App.js
@@ -1,40 +1,18 @@
 import React from 'react';
-import PropTypes from "prop-types";
-
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("hello");
-  }
   state = {
-    count: 0
-  }
-  add = () => {
-    this.setState(current => ({count:current.count+1}));
-  }
-  minus = () => {
-    this.setState(current => ({count:current.count-1}));
+    isLoading: true,
+    movies: []
   }
   componentDidMount() {
-    console.log("Component rendered");
-  }
-  componentDidUpdate() {
-    console.log("I just updated");
-  }
-  componentWillUnmount() {
-    console.log("Good bye");
+    setTimeout(() => {
+      this.setState({isLoading: false})
+    },6000)
   }
   render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>Hy! {this.state.count} </h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    )
+    const {isLoading} = this.state;
+    return <div>{isLoading ? "Loading..." : "We ary ready"}</div>
   }
 }
-
 export default App;
