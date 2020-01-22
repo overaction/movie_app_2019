01486c60e3648b7c5a0527314aeb623250f3f725
overaction <kmc04@naver.com>
2020-01-22 16:48:11 +0900

    css styling(modified)

--- src/Movie.js
+++ src/Movie.js
@@ -3,19 +3,24 @@ import PropTypes from "prop-types";
 import "./Movie.css";
 
 
-function Movie({id,year,title,summary,poster,genres}) {
-    return <div className="movie">
+function Movie({year,title,summary,poster,genres}) {
+    return ( 
+    <div className="movie">
         <img src={poster} alt={title} title={title}></img>
-        <div className="movie_data"></div>
-        <h3 className="movie_title">{title}</h3>
-        <h5 className="movie_year">{year}</h5>
-        <p className="movie_summary">{summary}</p>
-        <ul className="movie_genres">
-            {genres.map((genre,index) => ( //In map, index gives you number ex) 0 1 2...
-            <li key={index} className="genres_genre">{genre}</li>
+        <div className="movie_data">
+            <h3 className="movie_title">{title}</h3>
+            <h5 className="movie_year">{year}</h5>
+            <ul className="movie_genres">
+               {genres.map((genre,index) => ( //In map, index gives you number ex) 0 1 2...
+                <li key={index} className="genres_genre">{genre}
+                </li>
             ))}
         </ul>
+        <p className="movie_summary">{summary.slice(0,180)}...</p>
+        </div>
     </div>
+
+    )
 }
 
 Movie.prototype = {
