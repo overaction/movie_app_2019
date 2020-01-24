ba90bc63342c277842714507b9532e1e906873c8
overaction <kmc04@naver.com>
2020-01-24 14:25:51 +0900

    Building Router 2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {HashRouter, Route} from 'react-router-dom';
 import About from "./routes/About"
 import Home from "./routes/Home"
+import Navigation from "./components/Navigation"
 //export default 해준것은 import React처럼 괄호 없이 사용할 수 있지만
 //default를 붙이지 않은것은 {} 사용을 해야한다
 function App() {
   return <HashRouter>
+    <Navigation></Navigation>
     <Route path="/" exact={true} component={Home}></Route> 
     <Route path="/Abouts" component={About}></Route>
   </HashRouter>
